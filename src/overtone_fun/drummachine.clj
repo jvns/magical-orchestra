ebf8242ac1b1639d64df71f9d9b8d899a706a202
Julia Evans <julia@jvns.ca>
2013-11-19 17:35:15 -0500

    I HAVE A DRUM MACHINE

--- src/overtone_fun/drummachine.clj
+++ src/overtone_fun/drummachine.clj
@@ -1,7 +1,22 @@
 (ns overtone-fun.drummachine
   (:import jline.Terminal)
-;(:require [overtone.live :refer :all])  
+ (:require [overtone.live :refer :all])  
   (:gen-class))
+
+(definst c-hat [amp 0.8 t 0.04]
+  (let [env (env-gen (perc 0.001 t) 1 1 0 1 FREE)
+        noise (white-noise)
+        sqr (* (env-gen (perc 0.01 0.04)) (pulse 880 0.2))
+        filt (bpf (+ sqr noise) 9000 0.5)]
+    (* amp env filt)))
+
+
+(definst o-hat [amp 0.8 t 0.5]
+  (let [env (env-gen (perc 0.001 t) 1 1 0 1 FREE)
+        noise (white-noise)
+        sqr (* (env-gen (perc 0.01 0.04)) (pulse 880 0.2))
+        filt (bpf (+ sqr noise) 9000 0.5)]
+    (* amp env filt)))
 
 (defn read-character []
   (let [term (Terminal/getTerminal)]
@@ -11,6 +26,6 @@
   (while true
     (let [c (read-character)]
       (cond
-       (== c 68) (println "Left!")
-       (== c 67) (println "Right!")
+       (== c 68) (o-hat)
+       (== c 67) (c-hat)
        :else nil))))
